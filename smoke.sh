34c42ce471c3825c1b86469bb38a9ba17f441fcd
Ian Voysey <iev@cs.cmu.edu>
2017-02-16 14:03:34 -0500

    updating the syntax of times in the test actions to meet the API spec (and what is now getting parsed)

--- smoke.sh
+++ smoke.sh
@@ -6,7 +6,7 @@ echo
 echo
 
 echo "start"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-09T21:25:59.332087", "ARGUMENTS" : {}}' localhost:5000/action/start
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {}}' localhost:5000/action/start
 echo
 echo
 
@@ -16,21 +16,21 @@ echo
 echo
 
 echo "set battery"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-09T21:25:59.332087", "ARGUMENTS" : {"voltage" : 120}}' localhost:5000/action/set_battery
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"voltage" : 120}}' localhost:5000/action/set_battery
 echo
 echo
 
 echo "place obstacle"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-09T21:25:59.332087", "ARGUMENTS" : {"x" : 19.5, "y" : 11.5}}' localhost:5000/action/place_obstacle
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"x" : 19.5, "y" : 11.5}}' localhost:5000/action/place_obstacle
 echo
 echo
 
 echo "remove obstacle"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-09T21:25:59.332087", "ARGUMENTS" : {"obstacleid" : "Obstacle_0"}}' localhost:5000/action/remove_obstacle
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"obstacleid" : "Obstacle_0"}}' localhost:5000/action/remove_obstacle
 echo
 echo
 
 echo "perturb sensor"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-09T21:25:59.332087", "ARGUMENTS" : {"bump" : {"x" : 0, "y": 0, "z" : 0, "p" : 0, "w" : 0, "r" : 0}}}' localhost:5000/action/perturb_sensor
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"bump" : {"x" : 0, "y": 0, "z" : 0, "p" : 0, "w" : 0, "r" : 0}}}' localhost:5000/action/perturb_sensor
 echo
 echo
