7b1b09ae6ef0f8601592042dd7586406ade990b6
Ian Voysey <iev@cs.cmu.edu>
2017-02-17 14:21:16 -0500

    updating the TA to run on 8080, not the non-standard 5000, per #30

--- smoke.sh
+++ smoke.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
 
 echo "query path"
-curl localhost:5000/action/query_path
+curl localhost:8080/action/query_path
 echo
 echo
 
 echo "start"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {}}' localhost:5000/action/start
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {}}' localhost:8080/action/start
 echo
 echo
 
 echo "observe"
-curl localhost:5000/action/observe
+curl localhost:8080/action/observe
 echo
 echo
 
 echo "set battery"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"voltage" : 120}}' localhost:5000/action/set_battery
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"voltage" : 120}}' localhost:8080/action/set_battery
 echo
 echo
 
 echo "place obstacle"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"x" : 19.5, "y" : 11.5}}' localhost:5000/action/place_obstacle
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"x" : 19.5, "y" : 11.5}}' localhost:8080/action/place_obstacle
 echo
 echo
 
 echo "remove obstacle"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"obstacleid" : "Obstacle_0"}}' localhost:5000/action/remove_obstacle
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"obstacleid" : "Obstacle_0"}}' localhost:8080/action/remove_obstacle
 echo
 echo
 
 echo "perturb sensor"
-curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"bump" : {"x" : 0, "y": 0, "z" : 0, "p" : 0, "w" : 0, "r" : 0}}}' localhost:5000/action/perturb_sensor
+curl -H "Content-Type:application/json" -X POST -d '{"TIME" : "2017-02-16T17:35:39.539Z", "ARGUMENTS" : {"bump" : {"x" : 0, "y": 0, "z" : 0, "p" : 0, "w" : 0, "r" : 0}}}' localhost:8080/action/perturb_sensor
 echo
 echo
