32593be680991920bffce5440018c78589d22bc1
Ian Voysey <iev@cs.cmu.edu>
2017-03-07 10:42:40 -0500

    simplified permissions hack by doing it all at the top level and with sudo

--- installation/setup-cp1.sh
+++ installation/setup-cp1.sh
@@ -1,15 +1,10 @@
 #! /bin/bash
 
 # permissions hacks to deal with mystery losing exec
-cd catkin_ws/
-find . -name "*.py" | xargs chmod +x
-find . -name "*.sh" | xargs chmod +x
+sudo find . -name "*.py" | xargs sudo chmod +x
+sudo find . -name "*.sh" | xargs sudo chmod +x
 chmod +x /home/vagrant/bin/*
-cd ../
-chmod +x *.sh
-cd das/
-find . -name "*.sh" | xargs chmod +x
-cd ../
+
 source /opt/ros/indigo/setup.bash
 cd catkin_ws/src
 catkin_init_workspace
